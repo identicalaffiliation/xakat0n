4bc5e8042432e04069ff3012c7193b0a6025f7a9
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2026-08-04 18:37:00 +0300

    [KAN-3] consts fix

--- backend/internal/domain/status.go
+++ backend/internal/domain/status.go
@@ -5,8 +5,9 @@ type QueueStatus string
 const (
 	QueueStatusQueued    QueueStatus = "QUEUED"
 	QueueStatusOffered   QueueStatus = "OFFERED"
+	QueueStatusCheckout  QueueStatus = "CHECKOUT"
 	QueueStatusExpired   QueueStatus = "EXPIRED"
-	QueueStatusCompleted QueueStatus = "COMPLETED"
+	QueueStatusPurchased QueueStatus = "PURCHASED"
 	QueueStatusSoldOut   QueueStatus = "SOLD_OUT"
 	QueueStatusCancelled QueueStatus = "CANCELLED"
 )
